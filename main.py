24c88ea0f16946dda99445da50524878b2eb8150
Boris Tvaroska <boris@tvaroska.sk>
2024-09-06 20:23:03 +0000

    feat: layout

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ import flet as ft
 
 from vertexai.generative_models import GenerativeModel, Part
 
-def main(page: ft.Page):
+async def main(page: ft.Page):
 
     model = GenerativeModel(model_name="gemini-1.5-pro-001")
 
-    def get_file(e: ft.FilePickerResultEvent):
+    async def get_file(e: ft.FilePickerResultEvent):
         if e.files:
+            progress_ring.visible = True
+            progree_text.visible = True
             file = e.files[0]
 
             upload_file = ft.FilePickerUploadFile(
@@ -23,16 +25,16 @@ def main(page: ft.Page):
             )
 
             pick_file_dialog.upload([upload_file])
-            
-            text_part = Part.from_text('Summarize content of attached paper into three paragraphs')
-            with open(f'/tmp/flet/{file.name}', 'rb') as f:
+
+            text_part = Part.from_text("Summarize content of attached paper into three paragraphs")
+            with open(f"/tmp/flet/{file.name}", "rb") as f:
                 data = f.read()
-            pdf_part = Part.from_data(data, 'application/pdf')
+            pdf_part = Part.from_data(data, "application/pdf")
             response = model.generate_content(contents=[text_part, pdf_part])
-            selected_files.value = response.candidates[0].content.parts[0].text
+            model_response.value = response.candidates[0].content.parts[0].text
         else:
-            selected_files.value = "Cancelled!"
-        selected_files.update()
+            model_response.value = "Cancelled!"
+        model_response.update()
 
     page.title = "Gemini Reflection sample"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -40,31 +42,52 @@ def main(page: ft.Page):
     page.theme = ft.Theme(color_scheme_seed="blue")
 
     pick_file_dialog = ft.FilePicker(on_result=get_file)
-    pick_file_button = ft.IconButton(
+    pick_file_button = ft.ElevatedButton(
+                    text="Select PDF File",
                     icon=ft.icons.UPLOAD_FILE,
                     on_click=lambda _: pick_file_dialog.pick_files(
                         allow_multiple=True
                     ),
                 )
 
-    selected_files = ft.Text()
+    progress_ring = ft.ProgressRing(width=20, height=20, visible=False)
+    progree_text = ft.Text("Processing", visible=False)
+
+    file_column = ft.Column(
+            alignment=ft.alignment.top_left,
+            controls=[
+                pick_file_button,
+                ft.Row(controls = [progress_ring, progree_text])
+
+            ]
+        )
+
+    file_container = ft.Container(
+        height=page.height,
+        width=200,
+        content=file_column
+    )
+
+    model_response = ft.Text("Response will be here")
     page.overlay.append(pick_file_dialog)
 
     page.add(
         ft.AppBar(
             title=ft.Text("Gemini Reflection samples"),
             center_title=True,
-            actions=[pick_file_button]
+            # TODO: reset button
+#             actions=[pick_file_button]
         ),
-        ft.Row(
+        ft.ResponsiveRow(
             [
-                ft.Container(
-                    content = ft.Text('Hi')),
-                ft.VerticalDivider(width=9, thickness=3),
-                selected_files,
+                ft.Column(
+                    col=2,
+                    controls=[file_container]),
+                ft.Column(col=1, controls=[ft.VerticalDivider()]),
+                ft.Column(col=9, controls=[model_response])
             ]
         )
     )
 
-os.environ['FLET_SECRET_KEY'] = 'SECRET_KEY'
-ft.app(main, upload_dir='/tmp/flet', view=ft.AppView.WEB_BROWSER)
+os.environ["FLET_SECRET_KEY"] = "SECRET_KEY"
+ft.app(main, upload_dir="/tmp/flet", view=ft.AppView.WEB_BROWSER)
