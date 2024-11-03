785afe45eee7a5d982b353019f2c66448f13891a
Boris Tvaroska <boris@tvaroska.sk>
2024-11-03 22:03:37 +0000

    env variable PROJECT_ID

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,7 +2,8 @@ import { createVertex } from "@ai-sdk/google-vertex";
 import { createEdgeRuntimeAPI } from "@assistant-ui/react/edge";
 
 export const { POST } = createEdgeRuntimeAPI({
-
-  model: createVertex({project:'boris001', location:'us-central1'})("gemini-1.5-pro"),
-
+  model: createVertex({
+    project: process.env.PROJECT_ID,
+    location: 'us-central1'
+  })("gemini-1.5-flash")
 });
